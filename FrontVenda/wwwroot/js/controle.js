fd9cae7ac44b85a8c08c9d421a20038140060cc7
LaraAlmeida <lara.almeida@frgnet.com.br>
2023-02-10 17:44:34 -0300

    linkando o botão cadastrar

--- FrontVenda/wwwroot/js/controle.js
+++ FrontVenda/wwwroot/js/controle.js
@@ -1,3 +1,36 @@
-﻿function insereLinha(){
-    var lista = $(".lista").find("tbody");
-}
+﻿//var cadastro = document.querySelector("#botao-cadastrar");
+//cadastro.addEventListener("click", function (event) {
+//    event.preventDefault();
+//});
+
+//var navegadores = document.querySelectorAll(".nav-item");
+//navegadores.addEventListener("click");
+//for (i = 0; i < navegadores.length; i++) {
+//    if (navegadores[i].addEventListener("click")) { }
+
+//}
+//var enviar = document.querySelector
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
